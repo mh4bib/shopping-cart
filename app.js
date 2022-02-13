88995bcde0fd60e764a1eb55a537d07a0865b6d5
mahiuddinhabib <mh.habib137@gmail.com>
2022-02-13 17:44:01 +0600

    btn fuction added

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+function plusMinusBtn(product,isPlus,price){
+    const input = document.getElementById(product+'-input');
+    const priceElmnt = document.getElementById(product+'-price');
+    const subTotal = document.getElementById('sub-total');
+    const tax = document.getElementById('tax');
+    const total = document.getElementById('total');
+
+    if(isPlus == true){
+        inputValue = parseInt(input.value) + 1;
+    }
+    else if(isPlus == false && parseInt(input.value)>0){
+        inputValue = parseInt(input.value) - 1;
+    }
+    productTotal = inputValue * price;
+    input.value = inputValue;
+    priceElmnt.innerText = productTotal;
+
+    console.log(productTotal);
+}
 
 document.getElementById('phone-plus').addEventListener('click',function(){
     plusMinusBtn('phone', true, 1219);
